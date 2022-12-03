9a17b228034d8b32ba3fbfdab865d1572c84d07f
devAbu <abud_almonajed@hotmail.com>
2022-12-03 18:51:45 +0100

    Widget Tree

--- flutter_maxi_yt/lib/main.dart
+++ flutter_maxi_yt/lib/main.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
 
-void main() {
+/* void main() {
   runApp(MyApp());
-}
+} */
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer 1 chosen');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('Hello'),
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?"
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My first App'),
+        ),
+        body: Column(
+          children: [
+            const Text('The question!'),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: const Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Answer 2 chosen'),
+              child: const Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Answer 3 chosen');
+              },
+              child: const Text('Answer 3'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
